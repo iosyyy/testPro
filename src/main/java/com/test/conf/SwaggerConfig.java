ef08f591f9ec14e168e6bf1d4e058ae04ff2ae2b
iosyyy <626797813@qq.com>
2021-09-28 21:56:39 +0800

    #init 初始化项目结构

--- src/main/java/com/test/conf/SwaggerConfig.java
+++ src/main/java/com/test/conf/SwaggerConfig.java
@@ -34,10 +34,10 @@ public class SwaggerConfig {
   private ApiInfo webApiInfo() {
 
     return new ApiInfoBuilder()
-        .title("网站-课程中心API文档")
-        .description("本文档描述了课程中心微服务接口定义")
+        .title("还没想好要写啥的网站的api")
+        .description("test001")
         .version("1.0")
-        .contact(new Contact("jhx", "http://localhost.com", "test@qq.com"))
+        .contact(new Contact("Authoa", "http://localhost.com", "test@qq.com"))
         .build();
   }
 }
